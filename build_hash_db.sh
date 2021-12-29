53624e3ffcf3acfa1916c76f9eaa36bde8628434
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-12-29 17:05:36 +0100

    Keeping progress

--- build_hash_db.sh
+++ build_hash_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Copyright (c) 2021 José Manuel Barroso Galindo <theypsilon@gmail.com>
 
-set -euo pipefail
 ./build_hash_db.py
+RET=$?
+set -euo pipefail
 cat "${DB_FILE}"
 git config --global user.email "theypsilon@gmail.com"
 git config --global user.name "The CI/CD Bot"
@@ -10,3 +11,4 @@ git pull origin main
 git add "${DB_FILE}"
 git commit -m "${DB_FILE}" || true
 git push origin main
+exit ${RET}
